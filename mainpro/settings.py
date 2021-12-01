62b713b793bface106df4d0ef8ca2b81ebbcff74
Krishna2864 <75547302+Krishna2864@users.noreply.github.com>
2021-12-02 00:16:22 +0530

    Update settings.py

--- mainpro/settings.py
+++ mainpro/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,9 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-BASE_DIR / 'static']
-
+STATIC_ROOT = os.join(BASE_DIR,'staticfiles)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
