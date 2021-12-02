ddab12065a76a0c5992d669e3f7cb2550cf0e0e9
Krishna2864 <75547302+Krishna2864@users.noreply.github.com>
2021-12-02 16:10:25 +0530

    Update settings.py

--- mainpro/settings.py
+++ mainpro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@3t+)i03)--%g2si_vi@!=%pru^uy=qq3m33*h42ej%c%f9@38'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gossiphub.herokuapp.com']
 
